e189515ee3233a672c944d054d765955515ef911
Pablo de los Santos <estudiodedalo@gmail.com>
2023-03-09 20:50:49 -0300

    Clase 5 Tipos de datos

--- clase-05-tipos-de-datos-2.js
+++ clase-05-tipos-de-datos-2.js
@@ -26,4 +26,22 @@ let gato;
 console.log(gato); // "undefined"
 
 //Symbol
-//Los símbolos son un tipo de dato único en JavaScript que se utiliza para crear identificadores únicos.
+//Los símbolos son un tipo de dato único en JavaScript que se utiliza para crear identificadores únicos. Cada vez que se crea un símbolo, se genera un nuevo identificador único, lo que lo hace ideal para usar como claves de objetos o para identificar elementos de manera única en una aplicación.
+
+const simbolo = Symbol();
+
+let perrito = {
+    nombre: "Firulais",
+    edad: 3,
+    [simbolo]: "Identificador único" };
+
+console.log(perrito[simbolo]); // "Identificador único"
+
+//BigInt (Big interger)
+//Los bigint son un nuevo tipo de dato en JavaScript que se usa para representar números enteros de un tamaño mayor al que puede manejar JavaScript de manera nativa. Los bigint se escriben con el sufijo n, por ejemplo: 12345678901234567890n.
+
+const numeroGrande = 12345678901234567890n;
+
+console.log(numeroGrande + 1n); // 12345678901234567891n
+console.log(numeroGrande * 2n); // 2469135780246913578n
+console.log(numeroGrande / 3n); // 411218936707805260n
